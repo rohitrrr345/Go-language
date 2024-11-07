2fc418341a1a0ef03db63de1be27ff139fac5596
rohitrrr345 <rrajput2060@gmail.com>
2024-11-07 22:58:45 +0530

    Added user controllers

--- Var/MongoDb/Controllers/user.go
+++ Var/MongoDb/Controllers/user.go
@@ -6,6 +6,8 @@ import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"net/http"
+	"github.com/rohit/mongo-gplang/models"
+
 
 
 
@@ -20,6 +22,22 @@ func (uc UserController) GetUser (w http.ResponseWriter,r *http.Request,p httpro
 
 	id:=p.ByName("id")
 	if !bson.IsObjectIdHex(id){
-		
+		w.WriteHeader(http.StatusNotFound)
 	}
+	oid:=bson.ObjectHex(id)
+	u:=models.User{}
+	if err:=uc.Session.DB("mongo-golang").C('users').FindId(oid).One(&u);err!=nil{
+		w.WriteHeader((404))
+		return
+	}
+	uj,err:=json.Marshal((u))
+	if err!=nil{
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w,"%s\n",uj)
+	
+
+	
 }
