c92828f71a07d7c6597725816b5c1d5160343e19
rohitrrr345 <rrajput2060@gmail.com>
2024-11-08 00:55:41 +0530

    Added all controllers

--- Var/MongoDb/Controllers/user.go
+++ Var/MongoDb/Controllers/user.go
@@ -37,7 +37,38 @@ func (uc UserController) GetUser (w http.ResponseWriter,r *http.Request,p httpro
 	w.Header().Set("Content-Type","application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w,"%s\n",uj)
-	
+
 
 	
+}
+func (uc UserController) CreateUser(w http.ResponseWriter,r *http.Request ){
+	u:=models.User{}
+	json.NewDecoder(r.body).Decode(&u)
+	u.Id=bson.NewObjectId()
+	uc.session.DB("mongo-golang").C("Users").Insert(u)
+	uj,err:=json.Marshal(u)
+	if(err!=nil){
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w,"%s\n",uj)
+
+
+}
+func (uc UserController) DeleteUser(w http.ResponseWriter,r *http.Request,p httprouter.Params ){
+	id:=p.ByName("id")
+	if !bson.IsObjectIdHex(id){
+		w.WriteHeader(http.StatusNotFound)
+	}
+	oid:=bson.ObjectHex(id)
+	 if err:=uc.session.DB("mongo-golang").C("users").RemoveId(oid){
+		w.WriteHeader(404)
+	 }
+	 w.WriteHeader(http.StatusOK)
+	 fmt.Fprintf(w,"Deleted user",oid,"\n")
+
+
+
+
 }
